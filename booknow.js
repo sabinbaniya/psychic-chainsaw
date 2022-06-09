dccf401f7c38ee6d60a84092073edf71a3422f6a
sabinbaniya <dummymail@tutanota.de>
2022-06-09 16:48:29 +0545

    form validation little bit

--- booknow.js
+++ booknow.js
@@ -2,10 +2,14 @@ const checkInDate = document.getElementById("check-in-date");
 const checkOutDate = document.getElementById("check-out-date");
 
 checkInDate.addEventListener("change", handleChange);
+checkInDate.value = new Date().toLocaleDateString("en-CA");
 checkInDate.min = new Date().toLocaleDateString("en-CA");
-checkOutDate.disabled = true;
+const arr = checkInDate.min.split("-");
+arr[2] = parseInt(arr[2]) + 1;
+checkOutDate.min = arr.join("-");
 
 function handleChange() {
-  checkOutDate.disabled = false;
-  checkOutDate.min = checkInDate.value;
+  const arr = checkInDate.min.split("-");
+  arr[2] = parseInt(arr[2]) + 1;
+  checkOutDate.min = arr.join("-");
 }
