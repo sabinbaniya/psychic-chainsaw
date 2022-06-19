200c96c66d6e869e04b3d9f0e0a8f4386b00b8b3
sabinbaniya <dummymail@tutanota.de>
2022-06-19 16:49:19 +0545

    esewa things part 2 - complete

--- book.php
+++ book.php
@@ -1,16 +1,10 @@
 <?php
 include_once("./db/connectDB.php");
+include_once("./email_sender.php");
 
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
 
 //Load Composer's autoloader
 require 'vendor/autoload.php';
-
-// echo '<script>window.location.href = "./booking-completed.php"</script>';
 
 $name = $_GET["name"];
 $email = $_GET["email"];
@@ -83,8 +77,8 @@ if ($payment_option == "esewa") {
                 <input value="0" name="pdc" type="hidden">
                 <input value="EPAYTEST" name="scd" type="hidden">
                 <input value="ee2c3ca1-696b-4cc5-a6be-2c40d929d453" name="pid" type="hidden">
-                <input value="http://localhost/hotel/esewa_payment_failed.php?q=fu"  type="hidden" name="su">
-                <input value=' . $success_url . ' type="hidden" name="fu">
+                <input value=' . $success_url . '  type="hidden" name="su">
+                <input value="http://localhost/hotel/esewa_payment_failed.php?q=fu" type="hidden" name="fu">
                 <input value="Submit" type="submit" id="submit" type="hidden" style="display:none;">
             </form>
         </body>
@@ -97,6 +91,8 @@ if ($payment_option == "esewa") {
     return;
 }
 
+echo '<script>window.location.href = "./booking-completed.php"</script>';
+
 $stmt = $conn->prepare("INSERT INTO BOOKINGS (reservation_id , name, email, mobile, check_in_date, check_out_date, no_of_adults, no_of_children, no_of_rooms, payment_option, booking_status, payment_status, room_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 $stmt->bind_param("dssdssiiissss", $reservation_id, $name, $email, $mobile, $check_in_date, $check_out_date, $adults, $children, $no_of_rooms, $payment_option, $booking_status, $payment_status, $room_type);
 
@@ -105,179 +101,4 @@ $stmt->close();
 $conn->close();
 
 // for sending mail to customer after booking is done 
-
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
-
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp-mail.outlook.com';                       //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'baniya.sabinn@outlook.com';              //SMTP username
-    $mail->Password   = file_get_contents("google_password.txt"); //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-    $mail->Port       = 587;
-    $mail->SMTPSecure = "TLS";                                   //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-    //Recipients
-    $mail->setFrom('baniya.sabinn@outlook.com', 'Sabin Baniya');
-    $mail->addAddress($email, $name);     //Add a recipient    
-    $mail->addReplyTo('baniya.sabinn@outlook.com', 'Sabin Baniya');
-
-    // //Attachments
-    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Reservation Details regarding your booking from our website';
-    $mail->Body    = '
-            <head></head>
-            <body
-            style="
-                min-height: 50vh;
-                font-family: sans-serif;
-                margin: 10px auto;
-                max-width: 700px;
-            "
-            >
-            <p style="font-size: 18px; margin: 40px auto">
-                This email is regarding the booking you made online on
-                <a href="#" style="color: black">website name</a>. <br />
-                Please confirm your booking my clicking on the confirm booking button down
-                below.<br />
-                If you didn\'t made any bookings you can safely ignore this email. For any
-                enquiries contact our front desk at: 982343241
-            </p>
-            <div style="border: 2px solid black; border-radius: 10px">
-                <div
-                style="
-                    display: flex;
-                    align-items: center;
-                    justify-content: space-around;
-                    height: 200px;
-                "
-                >
-                <div
-                    style="
-                    display: flex;
-                    flex-direction: column;
-                    max-width: 150px;
-                    align-items: center;
-                    "
-                >
-                    <img
-                    src=""
-                    alt="Hotel Name"
-                    height="150px"
-                    />
-                    <p style="font-size: 20px">Hotel Name</p>
-                </div>
-                <div>
-                    <h1>Reservation Confirmed</h1>
-                    <p style="font-size: 16px">
-                    Your reservation number is :
-                    <span style="text-decoration: underline">' . $reservation_id . '</span>
-                    </p>
-                </div>
-                </div>
-            </div>
-            <div
-                style="
-                border: 2px solid black;
-                border-top: 0px;
-                border-radius: 10px;
-                padding: 0px 20px;
-                "
-            >
-                <div
-                style="
-                    margin: 20px 0px;
-                    display: flex;
-                    align-items: center;
-                    justify-content: space-between;
-                    width: 250px;
-                "
-                >
-                <img
-                    src=""
-                    alt="Hotel name"
-                    width="100px"
-                />
-                <div>
-                    <p style="font-size: 19px; font-weight: 600">Hotel Name</p>
-                    <p>Pokhara, Nepal</p>
-                    <p>Call us: 982149312</p>
-                </div>
-                </div>
-                <div>
-                <h2>Guest Name</h2>
-                <p style="font-size: 18px">' . $name . '</p>
-                <p style="font-size: 14px">' . $mobile . '</p>
-                </div>
-                <div style="display: flex">
-                <div style="margin-right: 20px">
-                    <p style="font-size: 18px">Check-in Date</p>
-                    <p style="font-size: 16px; color: #505050">' . $check_in_date . '</p>
-                </div>
-                <div style="margin-right: 20px">
-                    <p style="font-size: 18px">Check-Out Date</p>
-                    <p style="font-size: 16px; color: #505050">' . $check_out_date . '</p>
-                </div>
-                <div style="margin-right: 20px">
-                    <p style="font-size: 18px">Adult</p>
-                    <p style="font-size: 16px; color: #505050">' . $adults . '</p>
-                </div>
-                <div style="margin-right: 20px">
-                    <p style="font-size: 18px">Children</p>
-                    <p style="font-size: 16px; color: #505050">' . $children . '</p>
-                </div>
-                <div style="margin-right: 20px">
-                    <p style="font-size: 18px">Rooms</p>
-                    <p style="font-size: 16px; color: #505050">' . $no_of_rooms . '</p>
-                </div>
-                <div style="margin-right: 20px">
-                    <p style="font-size: 18px">Booking Status</p>
-                    <p style="font-size: 16px; color: #505050">' . $booking_status . '</p>
-                </div>
-                <div style="margin-right: 20px">
-                    <p style="font-size: 18px">Payment Status</p>
-                    <p style="font-size: 16px; color: #505050">' . $payment_status . '</p>
-                </div>
-                </div>
-            </div>
-            <div
-                style="
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                margin: 20px auto;
-                "
-            >
-                <a href="http://localhost/confirm-booking.php?id=' . $reservation_id . '">
-                <button
-                    style="
-                    background-color: #3b82f6;
-                    border-radius: 10px;
-                    border-width: 0px;
-                    padding: 20px 30px;
-                    color: white;
-                    font-weight: 600;
-                    font-size: 18px;
-                    "
-                    type="button"
-                >
-                    Confirm Booking
-                </button>
-                </a>
-            </div>
-            </body>
-
-        ';
-    $mail->AltBody = 'Please use a client-mail that supports html';
-    $mail->send();
-} catch (Exception $e) {
-    echo ($mail->ErrorInfo);
-}
+send_confirmation_email($name, $email, $reservation_id, $mobile, $check_in_date, $check_out_date, $adults, $children, $no_of_rooms, $booking_status, $payment_status, $room_type, $payment_option);
--- esewa_payment_success.php
+++ esewa_payment_success.php
@@ -1,3 +1,56 @@
 <?php
+include_once("./db/connectDB.php");
+include_once("./email_sender.php");
 
-echo "Success";
+$rsv_id = $_GET["id"];
+if (!isset($_GET["id"])) {
+    header("Location: ./index.php");
+    exit();
+}
+
+$rsv_id = doubleval($rsv_id);
+
+//after esewa's transaction is completed change the booking status to verified and payment status to verified and send mail to the customer.
+
+$booking_status = "verified";
+$payment_status = "verified";
+
+$stmt = $conn->prepare("UPDATE bookings SET booking_status = ? , payment_status = ? WHERE reservation_id = ?");
+$stmt->bind_param("ssd", $booking_status1, $payment_status1, $rsv_id);
+$stmt->execute();
+$stmt->store_result();
+
+$stmt = $conn->prepare("SELECT name, email, mobile, check_in_date, check_out_date, no_of_adults, no_of_children, no_of_rooms, payment_option, room_type FROM bookings WHERE reservation_id = ? LIMIT 1");
+$stmt->bind_param("d", $rsv_id);
+$stmt->execute();
+$stmt->store_result();
+$stmt->bind_result($name, $email, $mobile, $check_in_date, $check_out_date, $adults, $children, $no_of_rooms,  $room_type, $payment_option);
+$stmt->fetch();
+send_confirmation_email($name, $email, $rsv_id, $mobile, $check_in_date, $check_out_date, $adults, $children, $no_of_rooms, $booking_status, $payment_status, $payment_option, $room_type);
+
+$stmt->close();
+$conn->close();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Payment Successful</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body class="min-h-screen flex justify-center items-center">
+    <div class="text-center px-8">
+        <h1 class="text-3xl font-semibold my-4">Your Booking has been completed successfully.</h1>
+        <p class="text-xl my-4">You will recieve a mail shortly with your reservation details.</p>
+        <p class="text-gray-500">Didn't see a mail from us ? Try checking spam folder</p>
+        <a href="./index.php" class="mt-4 inline-block"><span class="underline underline-offset-2">Return Home</span></a>
+    </div>
+</body>
+
+</html>
